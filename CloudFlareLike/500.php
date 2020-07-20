423b536162fa87580d6cfeb06821641d08c3337a
Tassilo <38081490+TASSIA710@users.noreply.github.com>
2020-07-20 13:04:11 +0200

    Create 500.php

---
+++ CloudFlareLike/500.php
@@ -0,0 +1,22 @@
+<?php
+
+$error_code = 500;
+$error_name = 'Internal Server Error';
+$error_client = false;
+
+
+
+$error_host = 'Error';
+$error_document = null;
+
+
+
+$error_message = <<<EOT
+The server encountered an unexpected condition that prevented it from fulfilling your request.
+EOT;
+
+
+
+include(__DIR__ . '/base.php');
+
+?>

d2a1a847202d609f353929b7cef6e02f33287978
Tassilo <38081490+TASSIA710@users.noreply.github.com>
2020-07-20 13:03:03 +0200

    Create cf_datacenters.php

---
+++ CloudFlareLike/cf_datacenters.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+A list of common data centers and their respective IDs.
+Updated: 2020-06-21
+*/
+
+const DATA_CENTERS = [
+
+	// Africa
+	'TNR' => 'Antananarivo, Madagascar',
+	'CPT' => 'Cape Town, South Africa',
+	'CMN' => 'Casablanca, Morocco',
+	'DKR' => 'Dakar, Senegal',
+	'DAR' => 'Dar Es Salaam, Tanzania',
+	'JIB' => 'Djibouti City, Djibouti',
+	'DUR' => 'Durban, South Africa',
+	'JNB' => 'Johannesburg, South Africa',
+	'KGL' => 'Kigali, Rwanda',
+	'LOS' => 'Lagos, Nigeria',
+	'LAD' => 'Luanda, Angola',
+	'MPM' => 'Maputo, MZ',
+	'MBA' => 'Mombasa, Kenya',
+	'ROB' => 'Monrovia, Liberia',
+	'MRU' => 'Port Louis, Mauritius',
+	'RUN' => 'Réunion, France',
+
+	// Asia
+	// TODO
+
+	// Europe
+	'AMS' => 'Amsterdam, Netherlands',
+	'ATH' => 'Athens, Greece',
+	'BCN' => 'Barcelona, Spain',
+	'BEG' => 'Belgrade, Serbia',
+	'TXL' => 'Berlin, Germany',
+	'BRU' => 'Brussels, Belgium',
+	'OTP' => 'Bucharest, Romania',
+	'BUD' => 'Budapest, Hungary',
+	'KIV' => 'Chișinău, Moldova',
+	'CPH' => 'Copenhagen, Denmark',
+	'ORK' => 'Cork, Ireland',
+	'DUB' => 'Dublin, Ireland',
+	'DUS' => 'Düsseldorf, Germany',
+	'EDI' => 'Edinburgh, United Kingdom',
+	'FRA' => 'Frankfurt, Germany',
+	'GVA' => 'Geneva, Switzerland',
+	'GOT' => 'Gothenburg, Sweden',
+	'HAM' => 'Hamburg, Germany',
+	'HEL' => 'Helsinki, Finland',
+	'IST' => 'Istanbul, Turkey',
+	'KBP' => 'Kyiv, Ukraine',
+	'LIS' => 'Lisbon, Portugal',
+	'LHR' => 'London, United Kingdom',
+	'LUX' => 'Luxembourg City, Luxembourg',
+	'MAD' => 'Madrid, Spain',
+	'MAN' => 'Manchester, United Kingdom',
+	'MRS' => 'Marseille, France',
+	'MXP' => 'Milan, Italy',
+	'DME' => 'Moscow, Russia',
+	'MUC' => 'Munich, Germany',
+	'LCA' => 'Nicosia, Cyprus',
+	'OSL' => 'Oslo, Norway',
+	'CDG' => 'Paris, France',
+	'PRG' => 'Prague, Czech Republic',
+	'KEF' => 'Reykjavík, Iceland',
+	'RIX' => 'Riga, Latvia',
+	'FCO' => 'Rome, Italy',
+	'LED' => 'Saint Petersburg, Russia',
+	'SOF' => 'Sofia, Bulgaria',
+	'ARN' => 'Stockholm, Sweden',
+	'TLL' => 'Tallinn, Estonia',
+	'SKG' => 'Thessaloniki, Greece',
+	'VIE' => 'Vienna, Austria',
+	'VNO' => 'Vilnius, Lithuania',
+	'WAW' => 'Warsaw, Poland',
+	'ZAG' => 'Zagreb, Croatia',
+	'ZRH' => 'Zürich, Switzerland',
+
+	// Latin America & the Caribbean
+	// TODO
+
+	// Middle East
+	'AMM' => 'Amman, Jordan',
+	'BGW' => 'Baghdad, Iraq',
+	'GYD' => 'Baku, Azerbaijan',
+	'BEY' => 'Beirut, Lebanon',
+	'DOH' => 'Doha, Qatar',
+	'DXB' => 'Dubai, United Arab Emirates',
+	'KWI' => 'Kuwait City, Kuwait',
+	'BAH' => 'Manama, Bahrain',
+	'MCT' => 'Muscat, Oman',
+	'ZDM' => 'Ramallah',
+	'RUH' => 'Riyadh, Saudi Arabia',
+	'TLV' => 'Tel Aviv, Israel',
+
+	// North America
+	// TODO
+
+	// Oceania
+	'ADL' => 'Adelaide, SA, Australia',
+	'AKL' => 'Auckland, New Zealand',
+	'BNE' => 'Brisbane, QLD, Australia',
+	'MEL' => 'Melbourne, VIC, Australia',
+	'NOU' => 'Noumea, New caledonia',
+	'PER' => 'Perth, WA, Australia',
+	'SYD' => 'Sydney, NSW, Australia',
+
+];
